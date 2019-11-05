198dbf8b0b36c193b69fcf2bc4060caca5f6f3db
Shortty10 <peter.shortt10@gmail.com>
2019-11-05 20:42:15 +0000

    Added support for report metadata.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 def parse_report(url):
     """
-    Parse the report into a BeautifulSoup object.
+    Parse the report's HTML into a report object.
 
     Parameters
     -------------
@@ -18,11 +18,48 @@ def parse_report(url):
 
     Returns
     -------------
-    :class:`bs4.BeautifulSoup`
-        The Soup Object.
+    :class:`Report`
+        The report object.
     """
     r = requests.get(url).text
     if r == "Could not find any reports with that ID.":
         raise ValueError("Report not found.")
     soup = BeautifulSoup(r, "lxml")
-    return soup
+
+    judgement = soup.find("div", id="splash").text
+    if judgement == "This report has been closed without judgement.":
+        judgement = "Closed"
+    elif judgement == "This report has been deemed guilty.":
+        judgement = "Guilty"
+    elif judgement == "This report has been deemed innocent.":
+        judgement = "Innocent"
+
+    data = {}
+
+    data['judgement'] = judgement
+
+    data['id'] = soup.find("span", class_="reportId").text
+
+    data['user'] = soup.find("span", class_="reportedPlayer").text
+
+    data['reason'] = soup.find("span", class_="reportReason").text
+
+    data['ranked'] = bool("Ranked Game." in soup.find(
+        "span", class_="notice").text)
+
+    data['details'] = str(soup.find("span", class_='reportDescription')).split(
+        '<span class="reportDescription">')[1].split('</span>')[0].split("<br/>")
+
+    data['details'] = [] if data['details'] == [''] else data['details']
+
+    return Report(data)
+
+
+class Report:
+    def __init__(self, data):
+        self.id = data["id"]
+        self.user = data["user"]
+        self.reason = data["reason"]
+        self.details = data["details"]
+        self.ranked = data['ranked']
+        self.judgement = data['judgement']
